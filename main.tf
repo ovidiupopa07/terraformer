f4e89cff4734303aeeb3662f1d83b6330721e7c6
Ovidiu Popa <ovidiu.popa@armory.io>
2023-07-19 20:26:14 +0300

    Add tf code to create bucket

--- main.tf
+++ main.tf
@@ -2,9 +2,18 @@ provider "aws" {
     region = "us-west-2"
 }
 
-module "s3" {
-    source = "./s3"
-    # bucket name should be unique
-    bucket_name = "armory-terraformer-ovi-demo-2"
+module "s3_bucket" {
+    version = "3.10.1"
+    source = "terraform-aws-modules/s3-bucket/aws"
+    bucket = "ovidiu-terraformer-gtm"
+    block_public_acls = true
     object_ownership = "ObjectWriter"
+    block_public_policy = true
+    attach_deny_insecure_transport_policy	 = true
+    attach_require_latest_tls_policy = true
+    force_destroy = true
+    versioning = {
+        enabled = true
+    }
+
 }
--- s3/bucket.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "s3-demo-terraformer" {
-    bucket = "${var.bucket_name}"
-    acl = "${var.acl_value}"
-}
